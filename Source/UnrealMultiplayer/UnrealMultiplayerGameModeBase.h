63d939eedaf14505332f6adfffe72e32b1662483
Zr1RAM <sriram.varadarajulu@gmail.com>
2021-03-22 01:53:49 +0530

    Initial Commit without content folder as it currently only has starter content

---
+++ Source/UnrealMultiplayer/UnrealMultiplayerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealMultiplayerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALMULTIPLAYER_API AUnrealMultiplayerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
